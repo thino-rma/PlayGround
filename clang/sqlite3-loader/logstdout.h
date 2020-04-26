665a8327a07f7fdc862256b766b89b85d6fdee9f
thino-rma <thino-rma@yahoo.co.jp>
2020-04-26 15:15:36 +0000

    Add fflush()

--- clang/sqlite3-loader/logstdout.h
+++ clang/sqlite3-loader/logstdout.h
@@ -4,21 +4,21 @@
 #ifdef LOGSTDOUT
 /* output is enabled. */
 #define LOG_ERROR(...) printf("%s.%d [ERROR] ", __FILE__, __LINE__);\
-                       printf(__VA_ARGS__)
+                       printf(__VA_ARGS__); fflush(stdout)
 #define LOG_INFO(...)  printf("%s.%d [INFO] ", __FILE__, __LINE__);\
-                       printf(__VA_ARGS__)
+                       printf(__VA_ARGS__); fflush(stdout)
 #define LOG_DEBUG(...) printf("%s.%d [DEBUG] ", __FILE__, __LINE__);\
-                       printf(__VA_ARGS__)
+                       printf(__VA_ARGS__); fflush(stdout)
 #define LOG_DEBUG_IF(status, ...) if (status) {\
                            printf("%s.%d [DEBUG] ", __FILE__, __LINE__);\
-                           printf(__VA_ARGS__);\
+                           printf(__VA_ARGS__); fflush(stdout);\
                        }
 #define __LOG_CLOCK_INIT       clock_t __start_clock = 0, __delta_clock = 0;
 #define __LOG_CLOCK_START      __start_clock = clock();
 #define __LOG_CLOCK_END        __delta_clock = clock() - __start_clock;
 #define LOG_DEBUG_CLOCK(...) printf("%s.%d [DEBUG] ", __FILE__, __LINE__);\
  printf("spent %4.2f sec. ", __delta_clock / (double)CLOCKS_PER_SEC);\
-                               printf(__VA_ARGS__)
+                               printf(__VA_ARGS__); fflush(stdout)
 #else
 /* output is disabled. */
 #define LOG_ERROR(...)       /* LOG_ERROR()    */
