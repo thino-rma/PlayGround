126c988d56d2cdcc7091400fb095c935e37f52db
thino-rma <38564478+thino-rma@users.noreply.github.com>
2020-11-09 11:52:52 +0900

    Update setup_shell_env.sh

--- shell/setup_shell_env.sh
+++ shell/setup_shell_env.sh
@@ -145,9 +145,14 @@ download ${RAW_GIT}/${TARGET} ${VIM_DIR}/${TARGET}
 echo "### commands to instal packages ###"
 if [ $distri_name = "debian" ] || [ $distri_name = "ubuntu" ]; then
   echo "sudo apt -y install curl wget vim tmux"
+  echo "sudo localectl set-locale LANG=ja_JP.utf8"
+  echo "sudo timedatectl set-timezone Asia/Tokyo"
 fi
 if [ $distri_name = "redhat" ]; then
   echo "# curl -L https://copr.fedorainfracloud.org/coprs/unixcommunity/vim/repo/epel-7/unixcommunity-vim-epel-7.repo -o /etc/yum.repos.d/unixcommunity-vim-epel-7.repo"
   echo "sudo yum -y install curl wget vim tmux"
+  echo "sudo dnf -y install curl wget vim tmux"
+  echo "sudo localectl set-locale LANG=ja_JP.utf8"
+  echo "sudo timedatectl set-timezone Asia/Tokyo"
+  echo "sudo ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime"
 fi
-
