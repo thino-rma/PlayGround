ef2b82158bb54f0d63506cd72d1b1b069f941cb1
thino-rma <38564478+thino-rma@users.noreply.github.com>
2021-08-13 14:43:12 +0900

    Update start_env.sh

--- docs/python/start_env.sh
+++ docs/python/start_env.sh
@@ -1,7 +1,7 @@
 # alias conda_xxxx='cd ~/python_work/xxxx; . start_env.sh'
 # alias start_jupyter='jupyter-notebook --ip=0.0.0.0 --port=8888'
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     _CURRENT_DIR=`pwd`
     TARGET_ENV=${_CURRENT_DIR##*/}
 else
@@ -12,36 +12,36 @@ else
     fi
 fi
 
-if [[ $0 != "-bash" ]]; then
+if [[ "$0" != "-bash" ]]; then
     echo "don't execute this script in subshell"
     echo "$ source $0"
     exit 11
 fi
 
-if [ ${CONDA_SHLVL} == "2" ]; then
+if [ "${CONDA_SHLVL}" == "2" ]; then
     echo "current env: ${CONDA_DEFAULT_ENV}"
 else
-    if [ ${CONDA_SHLVL} == "0" ]; then
+    if [ "${CONDA_SHLVL}" == "0" ]; then
         echo "conda activate base"
         conda activate base
-        if [ ${CONDA_SHLVL} != "1" ]; then
+        if [ "${CONDA_SHLVL}" != "1" ]; then
             echo "### conda not found?"
             echo "type conda"
             type conda
         fi
     fi
 
-    if [ ${CONDA_SHLVL} == "1" ]; then
+    if [ "${CONDA_SHLVL}" == "1" ]; then
         echo "conda activate ${TARGET_ENV}"
         conda activate ${TARGET_ENV}
-        if [ ${CONDA_SHLVL} != "2" ]; then
+        if [ "${CONDA_SHLVL}" != "2" ]; then
             echo "### env not found: ${TARGET_ENV}"
             echo "conda env list"
             conda env list
         fi
     fi
 
-    if [ ${CONDA_SHLVL} == "2" ]; then
+    if [ "${CONDA_SHLVL}" == "2" ]; then
         # jupyter-notebook --ip=0.0.0.0 --port=8888
         :
     fi
